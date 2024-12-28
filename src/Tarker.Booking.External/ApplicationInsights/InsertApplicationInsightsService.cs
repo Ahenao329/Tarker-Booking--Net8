0febcbc19b538a731d76c7424bc5c126df43a12d
Ahemao <andersonhenao329@gmail.com>
2024-12-28 17:06:31 -0500

    m

---
+++ src/Tarker.Booking.External/ApplicationInsights/InsertApplicationInsightsService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Tarker.Booking.Application.External.ApplicationInsights;
+using Tarker.Booking.Domain.Models.ApplicationInsights;
+
+namespace Tarker.Booking.External.ApplicationInsights
+{
+    public class InsertApplicationInsightsService: IInsertApplicationInsightsService
+    {
+        private readonly IConfiguration _configuration;
+        public InsertApplicationInsightsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public bool Execute(InsertApplicationInsightsModel metric)
+        {
+            if(metric == null)
+                throw new ArgumentNullException(nameof(metric));
+
+            TelemetryConfiguration config = new TelemetryConfiguration();
+            config.ConnectionString = _configuration["ApplicationInsightsConnectionString"];
+
+            var _telemetryClient = new TelemetryClient(config);
+
+            var properties = new Dictionary<string, string>
+            {
+                {"Id", metric.Id},
+                {"Content", metric.Content },
+                {"Detail", metric.Detail }
+            };
+
+            _telemetryClient.TrackTrace(metric.Type, SeverityLevel.Information, properties);
+            return true;
+        }
+    }
+}
